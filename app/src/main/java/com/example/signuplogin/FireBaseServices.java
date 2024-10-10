05ab699abd2a0ff9507f91a64db2a17c1d2e41d3
mohamadtaha <tahamohamadd947@gmail.com>
2024-10-10 20:41:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/signuplogin/FireBaseServices.java
@@ -0,0 +1,44 @@
+package com.example.signuplogin;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+
+public class FireBaseServices
+{
+
+    private static FireBaseServices instance;
+    private FirebaseAuth auth;
+    private FirebaseFirestore fire;
+    private FirebaseStorage storage;
+
+    public FireBaseServices()
+    {
+        auth=FirebaseAuth.getInstance();
+        fire=FirebaseFirestore.getInstance();
+        storage=FirebaseStorage.getInstance();
+    }
+
+    public FirebaseStorage getStorage() {
+        return storage;
+    }
+
+    public FirebaseFirestore getFire() {
+        return fire;
+    }
+
+    public FirebaseAuth getAuth() {
+        return auth;
+    }
+
+    public static FireBaseServices getInstance()
+    {
+        if(instance==null) {
+            instance = new FireBaseServices();
+        }
+        return instance;
+    }
+
+
+
+}
